a10eef80a533e47bf1924f83d79745ed3079f969
bym1shaggybark <114554364+bymyshagg1bark@users.noreply.github.com>
2023-01-11 11:46:59 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -1,1 +1,11 @@
-
+def f5():
+  for i in range(516):
+    a = bin(i)[2::1]
+    if i % 2 == 0:
+        a = a + '10'
+    elif i % 2 != 0:
+        a = '1' + a + '01'
+    if int(a, 2) > 516:
+        print(int(a, 2))
+        break
+    
