aecb710ce5339ef7e487dce7d0d66b0b321df6b9
bym1shaggybark <114554364+bymyshagg1bark@users.noreply.github.com>
2023-01-11 12:00:02 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -8,4 +8,17 @@ def f5():
     if int(a, 2) > 516:
         print(int(i))
         break
+def f6():
+  from turtle import *
+left(90)
+for i in range(7):
+    forward(300)
+    right(120)
+pu()
+for x in range(1,9):
+    for y in range(1,10):
+        goto(x*30,y*30)
+        dot(2)
+done()
+
     
