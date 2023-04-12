5481c7dc399e0a5735a55abe2cb9be77d215f03c
bym1shaggybark <114554364+bymyshagg1bark@users.noreply.github.com>
2023-04-12 11:56:14 +0700

    Update 27 zadanie.py

--- 2полугодие/27 zadanie.py
+++ 2полугодие/27 zadanie.py
@@ -1,6 +1,12 @@
 with open('27_A.txt') as f:
-    d = []
     s = [x for x in f]
     s.pop(0)
+    sp = []
     for i in s:
-        d.append(i.split())
+        sp.append(list(map(int, i.split())))
+    for i in range(len(sp)):
+        if sp[i][1] % 36 == 0:
+            sp[i][1] = sp[i][1] // 36
+        else:
+            sp[i][1] = (sp[i][1] // 36) + 1
+    print(sp) 
